583c3064a1c1484090194db82be7124e1fdb4532
dkarakay <androidkarakay@gmail.com>
2018-06-06 17:48:48 +0300

    Arkanoid

---
+++ Assets/Scripts/RacketController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour {
+
+    public float speed = 125f;
+
+	void Update () {
+
+        float button = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * button * speed;	
+	}
+}
